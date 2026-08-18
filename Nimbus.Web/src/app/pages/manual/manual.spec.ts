4d11409aed7cd0404734bb3abb1b6c8dbdf2bfe7
NickThys3012 <nick.thys@vlaanderen.be>
2026-08-18 09:14:48 +0200

    fix ut

--- Nimbus.Web/src/app/pages/manual/manual.spec.ts
+++ Nimbus.Web/src/app/pages/manual/manual.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Manual } from './manual';
+import Manual from './manual';
 
 describe('Manual', () => {
   let component: Manual;
