748b7bf58c786c58ac5bc71f00b387ef687d7a37
NickThys3012 <nick.thys@vlaanderen.be>
2026-08-24 21:00:12 +0200

    fix 2.0

--- Nimbus.Web/src/app/Nimbus.spec.ts
+++ Nimbus.Web/src/app/Nimbus.spec.ts
@@ -3,8 +3,18 @@ import { provideRouter } from '@angular/router';
 import { Nimbus } from './Nimbus';
 import { LOCAL_STORAGE } from './services/theme-service';
 
+const store: Record<string, string> = {};
 const mockStorage = {
-  getItem: () => null,
+  getItem: (key: string) => store[key] ?? null,
+  setItem: (key: string, value: string) => {
+    store[key] = value;
+  },
+  removeItem: (key: string) => {
+    delete store[key];
+  },
+  clear: () => {
+    Object.keys(store).forEach((key) => delete store[key]);
+  },
   length: 0,
   key: () => null,
 } as unknown as Storage;
